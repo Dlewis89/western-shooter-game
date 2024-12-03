2641e11ca4d84a66105a88424147117f1daaaa1e
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-12-03 03:55:30 -0500

    feat: import assets functionality and display player sprite

--- player.py
+++ player.py
@@ -1,19 +1,39 @@
 import pygame
 from pygame.math import Vector2
+from os import walk
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, path, collision_sprites):
         super().__init__(groups)
-        self.image = pygame.Surface((100,100))
-        self.image.fill('red')
+
+        self.import_assets(path)
+        self.frame_index = 0
+        self.status = 'down_idle'
+
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center = pos)
 
         self.pos = Vector2(self.rect.center)
         self.direction = Vector2()
         self.speed = 200
-
         self.hitbox = self.rect.inflate(0, -self.rect.height / 2)
         self.collision_sprites = collision_sprites
+
+    def import_assets(self,path):
+        self.animations = {}
+
+        for index,folder in enumerate(walk(path)):
+            if index == 0:
+                for name in folder[1]:
+                    self.animations[name] = []
+            else:
+                for file_name in sorted(folder[2], key = lambda string: int(string.split('.')[0])):
+                    path = f"{folder[0].replace('\\','/')}/{file_name}"
+                    surf = pygame.image.load(path).convert_alpha()
+                    key = folder[0].split('/')[-1]
+                    self.animations[key].append(surf)
+
+        print(self.animations)
 
     def input(self):
         keys = pygame.key.get_pressed()
